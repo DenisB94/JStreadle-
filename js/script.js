05cec5afa64493f158300ab593de15d22c0a8074
DenisB94 <70068410+DenisB94@users.noreply.github.com>
2021-01-01 19:18:33 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,48 @@
+let t1, t2;
+
+let start = false;
+
+const engine = new Audio('./audio/engine.mp3');
+const audio = new Audio('./audio/car.mp3');
+engine.loop = true;
+engine.volume = 0.2;
+
+document.querySelector('.start').addEventListener('click', function () {
+    if (start === false) {
+        start = true;
+        this.innerHTML = 'stop';
+        engine.play(); 
+        document.querySelector('.treadle').addEventListener('click', pushTreadle);
+        document.querySelector('.progress-line').style.width = '100px'; 
+    }
+    else {
+        start = false;
+        this.innerHTML = 'start';
+        engine.pause(); 
+        document.querySelector('.treadle').removeEventListener('click', pushTreadle);
+        document.querySelector('.progress-line').style.width = '0'; 
+        t1 = clearTimeout(t1);
+        t1 = clearTimeout(t2);
+        audio.pause();
+        audio.currentTime = 0
+    }
+});
+
+function pushTreadle() {
+    t1 = clearTimeout(t1);
+    t1 = clearTimeout(t2);
+    this.classList.add('treadle-push');
+    document.querySelector('.progress-line').style.width = '250px'; 
+    audio.play();
+    stopTreadle();
+}
+function stopTreadle() {
+    t1 = setTimeout(() => {
+        document.querySelector('.treadle').classList.remove('treadle-push');
+        document.querySelector('.progress-line').style.width = '100px'; 
+    }, 500);
+    t2 = setTimeout(() => {
+        audio.pause();
+        audio.currentTime = 0;
+    }, 2000);
+}
